4d4f3e2bee9df92d4689bf66326cd5c16353df8c
Opilane TTHK <opilane@tthk.ee>
2020-09-08 11:59:27 +0300

    StringInterpolation

---
+++ StringInter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StringInter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FirstName;
+            string LastName;
+            Console.WriteLine("What is your first name?");
+            FirstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            LastName = Console.ReadLine();
+
+            //Console.WriteLine("Nice to meet you " + FirstName + " " + LastName + ".");
+
+            Console.WriteLine($"Nice to meet you {FirstName} {LastName}");
+
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FirstName;
+            string LastName;
+            string UserInput;
+
+            Console.WriteLine("What is your first name?");
+            FirstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            LastName = Console.ReadLine();
+            Console.WriteLine("When were you born?");
+            UserInput = Console.ReadLine();
+            int Year;
+            Year = Int32.Parse(UserInput);
+            int Age = 2020 - Year;
+
+            Console.WriteLine($"Greetings {FirstName} {LastName}, Age {Age}");
+
+
+
+        }
+    }
+}
